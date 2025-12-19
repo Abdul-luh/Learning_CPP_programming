ae06ef2f4030c14c898ea3fe8529a03343c0285e
Abdulluh <abdulluhodulate@gmail.com>
2025-12-19 07:33:29 +0100

    learned While loops

---
+++ l6/for_loops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "\nThis is a For loop" << endl;
+
+    for (int i =1; i <= 10; i++) {
+        cout << i << endl;
+    };
+
+    cout << "For loop is over" << endl;
+
+    return 0;
+
+}
